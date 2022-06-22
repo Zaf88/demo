993d6e25a37ffb4b5d09880726e692960cd78d5e
Your Name <applezafar@gmail.com>
2022-06-22 22:53:20 +1000

    HOMEWORK 2.2

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 27;
+        int c = 44;
+        int d = 15;
+        int e = 9;
+        int result = a * (b + (c - d * e));
+        System.out.println(result * -1);
+
+
+        int a1 = 5;
+        int b1 = 7;
+        int a2 = b1;
+        int b2 = (a1 + a1 + a1 + a1 + a1) / a1;
+        System.out.println(a2);
+        System.out.println(b2);
+
+
+    int a3 = 156;
+    int x1 = 1;
+    int x2 = 5;
+    int x3 = 6;
+    int b3 = x2;
+        System.out.println(b3);
+}}
--- src/Vessel.java
+++
@@ -1,6 +0,0 @@
-public class lVessel implements Truck {
-
-    @Override
-    public String transportCargo() {
-        return "boxes";
-    }}
